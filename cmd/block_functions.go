3ede42fb6f5bbef21aa129cd26cf6f58d1f5597e
tdadadavid <davidtofunmidada@gmail.com>
2025-04-01 01:20:33 +0100

    rework the blockchain helper functions

--- cmd/block_functions.go
+++ cmd/block_functions.go
@@ -5,21 +5,23 @@ import (
 	"fmt"
 
 	"github.com/tdadadavid/block/pkg/chain"
+	"github.com/tdadadavid/block/pkg/transactions"
 )
 
 var blockChain chain.Chain
+var chainStorePath = "/data/blocks"
 
 func init() {
-	blockChain = chain.New(context.Background(), "/data/blocks")
+	blockChain = chain.New(context.Background(), chainStorePath)
 }
 
 func addBlock(data string) {
-	blockChain.AddBlock(data)
+	blockChain.AddBlock(transactions.Transaction{}) //TODO: fix me
 }
 
 func printBlock(hash string) {
 	blockChain.PrintBlock(hash)
-} 
+}
 
 func printChain(_ string) {
 	blockChain.PrintChain()
